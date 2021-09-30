fe98717268c968c93db68ac747ef090e048f1b08
MOHIT YADAV <mohityadav050575@gmail.com>
2021-09-30 20:53:21 +0530

    Update pb_RMX3031.mk

--- pb_RMX3031.mk
+++ pb_RMX3031.mk
@@ -17,7 +17,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
